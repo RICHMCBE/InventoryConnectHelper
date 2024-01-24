048e7e95f729493ce239c7d451aca924c4a540e1
bluefirewolf534 <bluefirewolf534@gmail.com>
2024-01-24 19:42:03 +0900

    :tada: project started

--- src/romo/inventoryconnect/InventoryConnect.java
+++ src/romo/inventoryconnect/InventoryConnect.java
@@ -1,8 +1,12 @@
+package romo.inventoryconnect;
+
 import alemiz.stargate.StarGate;
 import dev.waterdog.waterdogpe.plugin.Plugin;
+import romo.inventoryconnect.protocol.InventorySavePacket;
 
-public class Main extends Plugin {
+public class InventoryConnect extends Plugin {
     @Override
     public void onEnable(){
+        StarGate.getInstance().getServer().getProtocolCodec().registerPacket((byte) 14, InventorySavePacket.class);
     }
 }
---
+++ src/romo/inventoryconnect/protocol/InventorySavePacket.java
@@ -0,0 +1,54 @@
+package romo.inventoryconnect.protocol;
+
+import alemiz.stargate.StarGate;
+import alemiz.stargate.handler.StarGatePacketHandler;
+import alemiz.stargate.protocol.StarGatePacket;
+import alemiz.stargate.protocol.types.PacketHelper;
+import io.netty.buffer.ByteBuf;
+
+public class InventorySavePacket extends StarGatePacket {
+
+    private int status;
+    private long xuid;
+
+    @Override
+    public void encodePayload(ByteBuf byteBuf) {
+        PacketHelper.writeInt(byteBuf, this.status);
+        PacketHelper.writeLong(byteBuf, this.xuid);
+    }
+
+    @Override
+    public void decodePayload(ByteBuf byteBuf) {
+        this.status = PacketHelper.readInt(byteBuf);
+        this.xuid = PacketHelper.readLong(byteBuf);
+    }
+
+    @Override
+    public byte getPacketId() {
+        return 14;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public long getXuid() {
+        return xuid;
+    }
+
+    public void setXuid(long xuid) {
+        this.xuid = xuid;
+    }
+
+    @Override
+    public boolean handle(StarGatePacketHandler handler) {
+        StarGate.getInstance().getSession()
+        System.out.println(this.status);
+        System.out.println(this.xuid);
+        return true;
+    }
+}
