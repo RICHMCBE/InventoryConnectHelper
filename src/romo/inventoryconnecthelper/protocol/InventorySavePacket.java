d2c1e42b053cc9d86ae04bbec252b722b79b6c85
bluefirewolf534 <bluefirewolf534@gmail.com>
2024-01-26 13:14:53 +0900

    :zap: InventorySavePacket 전송 클라이언트의 이름 전송

--- src/romo/inventoryconnecthelper/protocol/InventorySavePacket.java
+++ src/romo/inventoryconnecthelper/protocol/InventorySavePacket.java
@@ -4,20 +4,27 @@ import alemiz.stargate.handler.StarGatePacketHandler;
 import alemiz.stargate.protocol.StarGatePacket;
 import alemiz.stargate.protocol.types.PacketHelper;
 import io.netty.buffer.ByteBuf;
+import romo.inventoryconnecthelper.InventoryConnectHelper;
+
+import java.util.Iterator;
+import java.util.Objects;
 
 public class InventorySavePacket extends StarGatePacket {
 
+    private String clientName;
     private int status;
     private long xuid;
 
     @Override
     public void encodePayload(ByteBuf byteBuf) {
+        PacketHelper.writeString(byteBuf, this.clientName);
         PacketHelper.writeInt(byteBuf, this.status);
         PacketHelper.writeLong(byteBuf, this.xuid);
     }
 
     @Override
     public void decodePayload(ByteBuf byteBuf) {
+        this.clientName = PacketHelper.readString(byteBuf);
         this.status = PacketHelper.readInt(byteBuf);
         this.xuid = PacketHelper.readLong(byteBuf);
     }
@@ -25,6 +32,14 @@ public class InventorySavePacket extends StarGatePacket {
     @Override
     public byte getPacketId() {
         return 14;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
     }
 
     public int getStatus() {
@@ -45,8 +60,11 @@ public class InventorySavePacket extends StarGatePacket {
 
     @Override
     public boolean handle(StarGatePacketHandler handler) {
-        System.out.println(this.status);
-        System.out.println(this.xuid);
+        InventoryConnectHelper.getInstance().getInventoryConnectServerSessions().forEach((key, session) -> {
+            if(!Objects.equals(session.getSessionName(), this.clientName)){
+                session.sendPacket(this);
+            }
+        });
         return true;
     }
 }
